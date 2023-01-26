b863b8dffc5073ceaa9115085fd3771f817cb0d1
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-01-26 17:28:54 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import router from "./router/index.js";
 
-import { createImageKitVue } from "imagekit-vue3"
 
 
 loadFonts();
@@ -14,10 +13,5 @@ const app = createApp(App);
 
 app.use(vuetify)
   .use(router)
-  // .use(createImageKitVue, {
-  //   urlEndpoint: "https://ik.imagekit.io/quentinglx", // Required. Default URL-endpoint is https://ik.imagekit.io/your_imagekit_id
-  //   publicKey: "public_QoAnOszxzry0gVchb2sz3NhXi+w=", // optional
-  //   registerGlobalComponents: true, // optional. Default is false, this will register all ImageKitVue components globally
-  // });
 
 app.mount("#app");
