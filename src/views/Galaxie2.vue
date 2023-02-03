788778904c07a103956b9d1aecbe098f8b367784
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-02-03 08:08:30 +0100

    logic

--- src/views/Galaxie2.vue
+++ src/views/Galaxie2.vue
@@ -4,9 +4,11 @@ import { ref, watch, computed  } from "vue";
 
 const body = ref("");
 const data=ref();
+const dataMemory=ref([]);
 const headers =ref([]);
 const loading = ref(false);
 const prepublie = ref(false);
+var firstTime = false;
 
 
 fetchGalaxie(false);
@@ -31,6 +33,15 @@ function formatResponse(body) {
   var div = document.createElement("div");
   div.innerHTML = body;
   data.value = parsing(body);
+  
+  if (dataMemory.value.length == 0 ) {
+    dataMemory.value = data.value;
+    firstTime = true
+  }
+  else if (dataMemory.value.length > 0 && firstTime ){
+    dataMemory.value = dataMemory.value.concat(data.value)
+    firstTime = false
+  }
   loading.value = false;
 }
 
@@ -51,7 +62,7 @@ function parsing(body) {
     headersKey.push(lkey);
     headersText.push(lname);
     
-    if ( [ 'etablissement', 'reference_galaxie', 'article', 'corps', 'chaire', 'section', 'profil', 'localisation'].includes(lkey))
+    if ( [ 'etablissement', 'reference_galaxie', 'article', 'corps',  'section', 'profil', 'localisation'].includes(lkey))
    headers.value.push( {
         text   : lname,
         value  : lkey,
@@ -62,13 +73,13 @@ function parsing(body) {
   });
     headers.value[0]=  headers.value.splice(1, 1,  headers.value[0])[0]; // invert ref and etablissement
     headers.value[1].sortable=true; 
-    headers.value[5].sortable=true; 
-    headers.value[7].sortable=true; 
+    headers.value[4].sortable=true; 
+    headers.value[6].sortable=true; 
     headers.value[0].width=100; 
     headers.value[1].width=250; 
-    headers.value[6].width=250;  
-    headers.value[7].width=150;  
-    console.log(headers.value)
+    headers.value[5].width=250;  
+    headers.value[6].width=150;  
+    //console.log(headers.value)
         
 
   var rows = doc.querySelectorAll("table.tab tbody tr");
@@ -101,17 +112,29 @@ function parsing(body) {
 const searchField = ref(["section","etablissement","profil","section2","section3"]);
 const searchValue = ref("");
 const choiceCorps = ref(false);
+const choiceArticle = ref(false);
+const addFilter = ref(false);
 
 
 
 
 const filterOptions = computed(() => {
   const filterOptionsArray = [];
+  if (addFilter.value){
+    console.log('addFilter')
   filterOptionsArray.push ({
     field: 'corps',
     comparison: '=',
     criteria: choiceCorps.value ? 'PR' : 'MCF'
   })
+  if (choiceCorps.value) {
+    console.log(choiceCorps.value)
+  filterOptionsArray.push ({
+    field: 'article',
+    comparison: '=',
+    criteria: choiceArticle.value ? '46-3' : '46-1'
+  })
+}}
   return filterOptionsArray;
 })
 
@@ -128,30 +151,53 @@ const sortType = "asc";
   <div  class="flex justify-center p-1 bg-gradient-to-r from-fuchsia-900 to-fuchsia-400">
     <img class="w-80" src="@/assets/galaxie-min.png"/>
   </div>
-  <h2 class="p-3 font-bold"> {{ prepublie ? "Postes Pré-publiés" : "Postes Publiés" }} </h2>
+  <h2 class="p-3 font-bold"> {{ prepublie ? "Postes Pré-publiés" : "Postes Publiés" }} 
+  {{  !addFilter ? ''  :
+      !choiceCorps ? '- MCF' : 
+       choiceArticle  ? '- PR 46-3' : '- PR 46-1'  }} </h2>
   <div v-if="data" class=" "> 
 
-    <v-text-field class="w-2/6 pb-2"
+    <div class="flex"> 
+       <v-text-field class="w-1/8 pb-2"
         v-model="searchValue"
         prepend-icon="mdi-magnify"
         label="Sections / Université / Profil"
         single-line
         hide-details
       ></v-text-field> 
+      <v-switch 
+       v-model="prepublie"
+       inset
+       class="w-1/8 pb-2 pl-10" 
+       label="Poste publiés / Poste pré-publiés "> </v-switch> 
 
-       <div class="flex"> <v-switch 
+      
+       </div>
+       <div class="flex grow-0">
+        <div class="w-1/4 pb-2" > <v-switch 
+       v-model="addFilter"
+       inset
+       class=" pb-2 pl-10" 
+       label="Tout voir / filtres"> </v-switch> 
+      </div>
+
+       <div v-if=addFilter class="flex w-1/2"> <v-switch 
        v-model="choiceCorps"
        inset
-       class="pl-10" 
+       class="w-1/4 pb-2 pl-10" 
        label="MCF / PR"> </v-switch> 
 
        <v-switch 
-       v-model="prepublie"
+       v-model="choiceArticle"
+       v-if="choiceCorps"
        inset
-       class="pl-10" 
-       label="Poste publiés / Poste pré-publiés "> </v-switch> 
+       class="w-1/4 pb-2 pl-10" 
+       label="46-1 / 46-3"> </v-switch>
       
        </div>
+
+       </div>
+      
 
 
     <EasyDataTable
@@ -200,21 +246,21 @@ const sortType = "asc";
 
   --easy-table-header-font-size: 14px;
   --easy-table-header-height: 50px;
-  --easy-table-header-font-color: #c1cad4;
-  --easy-table-header-background-color: #2d3a4f;
+  --easy-table-header-font-color: #fcfcfc;
+  --easy-table-header-background-color: #7d91ae;
 
   --easy-table-header-item-padding: 10px 15px;
 
   --easy-table-body-even-row-font-color: #fff;
-  --easy-table-body-even-row-background-color: #4c5d7a;
+  --easy-table-body-even-row-background-color: #2b374b;
 
   --easy-table-body-row-font-color: #2d3a4f;
-  --easy-table-body-row-background-color: #c9deff;
+  --easy-table-body-row-background-color: rgb(248, 252, 255);
   --easy-table-body-row-height: 50px;
   --easy-table-body-row-font-size: 13px;
 
   --easy-table-body-row-hover-font-color: #2d3a4f;
-  --easy-table-body-row-hover-background-color: rgb(219, 234, 254);
+  --easy-table-body-row-hover-background-color: rgb(254, 254, 255);
 
   --easy-table-body-item-padding: 10px 15px;
 
